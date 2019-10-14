4046298232e8a45313ae228db323f8234028e40c
Nabil <nabilhz1994@gmail.com>
2019-10-14 04:04:51 +0200

    the 7 time

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,4 +137,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-DATABASES['default'] = dj_database_url.config(default='postgres://...'}
+ALLOWED_HOSTS=['*']
